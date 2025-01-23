c367fa9302c2bdc3915aae3b07419b98e4412fec
appitoriadev <contact@appitoria.dev>
2025-01-22 21:02:04 -0500

    Enhance joke rendering logic to ensure details are shown only once for "Two Part" jokes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Para resolver el ejercicio necesitamos:
 2. Renderizar el chiste en el HTML dependiendo de si es un "Two Part" o "Single"
     - Si es single, sólo deberíamos ver el chiste.
     - Si es un "Two Part" deberíamos ver la primera línea del chiste, seguido de un botón de detalle que me permita revelar la segunda parte del chiste.
+    - Sólo se debe de mostrar el detalle del chiste una vez.
     - Cada uno de los botones debe de tener una función definida.
 3. Mejorar el diseño sin afectar la funcionalidad del aplicativo. Use su creatividad. Puede utilizar librerías externas.
     
