1c64ac87151ff744255151d34aedae4d54243675
appitoriadev <contact@appitoria.dev>
2025-01-29 01:49:00 -0500

    Add initial instructions for the exercise in index.ts

---
+++ typescript/index.ts
@@ -0,0 +1,14 @@
+/*
+Para resolver el ejercicio necesitamos:
+1. Obtener el chiste.
+2. Renderizar el chiste en el HTML dependiendo de si es un "Two Part" o "Single"
+    - Si es single, sólo deberíamos ver el chiste.
+    - Si es un "Two Part" deberíamos ver la primera línea del chiste, seguido de un botón de detalle que me permita revelar la segunda parte del chiste.
+    - Sólo se debe de mostrar el detalle del chiste una vez.
+    - Cada uno de los botones debe de tener una función definida.
+3. Mejorar el diseño sin afectar la funcionalidad del aplicativo. Use su creatividad. Puede utilizar librerías externas.
+    
+Si existen dudas al respecto del ejercicio por favor diríjanlas a través del Discord.
+Adjunto dos imágenes básicas de como se ve el ejercicio final. 
+
+*/
